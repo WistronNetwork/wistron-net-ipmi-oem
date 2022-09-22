c1952c2ce80b809cd795684d93867deb5402d1d8
Neal_Chen <neal_chen@wistron.com>
2022-09-22 11:12:52 +0800

    kavalan_fru: Add kavalan FRU header
    Setup kavalan FRU eeprom path
    
    Signed-off-by: Neal_Chen <neal_chen@wistron.com>

---
+++ fru/kavalan_fru.hpp
@@ -0,0 +1,49 @@
+/*
+// Copyright (c) 2022 Wistron Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#pragma once
+
+#include <ipmid/api-types.hpp>
+
+#define FRU_MAX 9
+
+const char* frulist[FRU_MAX] = {
+    "/sys/bus/i2c/devices/0-0054/eeprom",  // fru 0: BMC
+    "/sys/bus/i2c/devices/1-0056/eeprom",  // fru 1: MB
+    "/sys/bus/i2c/devices/38-0051/eeprom", // fru 2: FAN1
+    "/sys/bus/i2c/devices/37-0055/eeprom", // fru 3: FAN2
+    "/sys/bus/i2c/devices/37-0053/eeprom", // fru 4: FAN3
+    "/sys/bus/i2c/devices/37-0051/eeprom", // fru 5: FAN4
+    "/sys/bus/i2c/devices/36-0055/eeprom", // fru 6: FAN5
+    "/sys/bus/i2c/devices/36-0053/eeprom", // fru 7: FAN6
+    "/sys/bus/i2c/devices/36-0051/eeprom"  // fru 8: FAN7
+};
+
+/*
+ *  This struct is for external sensor.
+ *  If there is no external sensor in the project, please keep struct empty.
+ */
+struct SensorParams
+{
+    /** @brief The sensor path quantity */
+    int size;
+    /** @brief dbus path */
+    std::string path;
+};
+
+struct SensorParams sensorparams[] = {
+    NULL,
+};
